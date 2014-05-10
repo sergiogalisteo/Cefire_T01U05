4eba83b50b62f7c6e22f8542477e6c4503321980
Sergio Galisteo <sergio.galisteo@gmail.com>
2014-05-11 00:10:11 +0200

    Añado mensaje despedida...

--- MiHolaMundo.java
+++ MiHolaMundo.java
@@ -2,6 +2,7 @@ public class MiHolaMundo {
     public static void main(String[] args) {
 	System.out.println("¡¡HOLA MUNDO!!");
 	System.out.println("¿Qué tal rama2?");
+	System.out.println("¡Adiosssss!");
     }
     
 }
